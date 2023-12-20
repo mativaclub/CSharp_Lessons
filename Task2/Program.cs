e024c4f070ae4a05a0e0f0f6418a0d7a7adacabf
Maria <mativaclub@gmail.com>
2023-12-20 18:47:43 +0200

    Added different variables in Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 5;
+int i2 = 2;
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 3;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int result1 = i1 * i2;
+long result2 = l1 * i1;
+double result3 = d1 * i1;
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
+Console.WriteLine(result3);
