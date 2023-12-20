7487397eb26ba5ca398038befbd310b5b21f3136
Maria <mativaclub@gmail.com>
2023-12-20 18:56:44 +0200

    Homework is done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -18,3 +18,14 @@ double result3 = d1 * i1;
 Console.WriteLine(result1);
 Console.WriteLine(result2);
 Console.WriteLine(result3);
+
+d1 = 6;
+double result4 = i1/d1;
+Console.WriteLine(result4);
+
+int a = 5;
+int b = 6;
+int c = 3;
+ind d = 2;
+int result = (a*b)/(c+d);
+Console.WriteLine(result)
